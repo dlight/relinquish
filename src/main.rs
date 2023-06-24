547a6ec2cf5b36dca4463fdc0c36b1e3fd1d4d8d
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2023-06-24 05:57:29 -0300

    still can't print the whole line on parser errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-//! An example of using logos with chumsky to parse sexprs
-//! Run it with the following command:
-//! cargo run --example logos
+use std::fmt;
 
 use ariadne::{Color, Label, Report, ReportKind, Source};
 use chumsky::{
@@ -8,7 +6,18 @@ use chumsky::{
     prelude::*,
 };
 use logos::Logos;
-use std::fmt;
+use makeit::{Buildable, Builder};
+
+type Span = SimpleSpan<usize>;
+type Spanned<T> = (T, Span);
+
+type ParserError<'tokens> = extra::Err<Rich<'tokens, Token>>;
+
+#[derive(Builder)]
+struct EvalError {
+    span: Span,
+    msg: String,
+}
 
 #[derive(Logos, Debug, Clone, PartialEq)]
 #[logos(skip r"[ \t\r\n]+")]
@@ -51,24 +60,14 @@ impl fmt::Display for Token {
 #[derive(Debug)]
 enum TokenTree {
     Token(Token),
-    Parens(Vec<Self>),
+    Parens(Vec<Spanned<Self>>),
 }
 
-// This function signature looks complicated, but don't fear! We're just saying that this function is generic over
-// inputs that:
-//     - Can have tokens pulled out of them by-value, by cloning (`ValueInput`)
-//     - Gives us access to slices of the original input (`SliceInput`)
-//     - Produces tokens of type `Token`, the type we defined above (`Token = Token<'a>`)
-//     - Produces spans of type `SimpleSpan`, a built-in span type provided by chumsky (`Span = SimpleSpan`)
-// The function then returns a parser that:
-//     - Has an input type of type `I`, the one we declared as a type parameter
-//     - Produces an `SExpr` as its output
-//     - Uses `Rich`, a built-in error type provided by chumsky, for error generation
-fn parser<'a, I>() -> impl Parser<'a, I, TokenTree, extra::Err<Rich<'a, Token>>>
+fn parser<'tokens, Input>() -> impl Parser<'tokens, Input, Spanned<TokenTree>, ParserError<'tokens>>
 where
-    I: ValueInput<'a, Token = Token, Span = SimpleSpan>,
+    Input: ValueInput<'tokens, Token = Token, Span = SimpleSpan>,
 {
-    recursive(|sexpr| {
+    recursive(|token_tree| {
         let atom = select! {
             Token::Float(x) => TokenTree::Token(Token::Float(x)),
             Token::Add => TokenTree::Token(Token::Add),
@@ -77,91 +76,116 @@ where
             Token::Div => TokenTree::Token(Token::Div),
         };
 
-        let list = sexpr
+        let list = token_tree
             .repeated()
             .collect()
             .map(TokenTree::Parens)
             .delimited_by(just(Token::LParen), just(Token::RParen));
 
-        atom.or(list)
+        atom.or(list).map_with_span(|expr, span| (expr, span))
     })
 }
 
 impl TokenTree {
-    // Recursively evaluate an s-expression
-    fn eval(&self) -> Result<f64, String> {
-        match self {
+    fn eval(expr: &Spanned<Self>) -> Result<f64, EvalError> {
+        let error = EvalError::builder().set_span(expr.1);
+
+        match &expr.0 {
             Self::Token(Token::Float(x)) => Ok(*x),
-            Self::Token(x) => Err(format!("Cannot evaluate operator {:?}", x)),
+            Self::Token(x) => Err(error
+                .set_msg(format!("Cannot evaluate operator {:?}", x))
+                .build()),
             Self::Parens(list) => match &list[..] {
-                [Self::Token(Token::Add), tail @ ..] => tail.iter().map(TokenTree::eval).sum(),
-                [Self::Token(Token::Mul), tail @ ..] => tail.iter().map(TokenTree::eval).product(),
-                [Self::Token(Token::Sub), init, tail @ ..] => {
-                    Ok(init.eval()? - tail.iter().map(TokenTree::eval).sum::<Result<f64, _>>()?)
+                [(Self::Token(Token::Add), _), tail @ ..] => tail.iter().map(TokenTree::eval).sum(),
+                [(Self::Token(Token::Mul), _), tail @ ..] => {
+                    tail.iter().map(TokenTree::eval).product()
                 }
-                [Self::Token(Token::Div), init, tail @ ..] => Ok(init.eval()?
+                [(Self::Token(Token::Sub), _), init, tail @ ..] => Ok(TokenTree::eval(init)?
+                    - tail.iter().map(TokenTree::eval).sum::<Result<f64, _>>()?),
+                [(Self::Token(Token::Div), _), init, tail @ ..] => Ok(TokenTree::eval(init)?
                     / tail
                         .iter()
                         .map(TokenTree::eval)
                         .product::<Result<f64, _>>()?),
-                _ => Err("Cannot evaluate list".to_owned()),
+                _ => Err(error.set_msg("Cannot evaluate list".to_owned()).build()),
             },
         }
     }
 }
 
-const SRC: &str = r"
+fn run_program(src: &str) {
+    println!("input program {}", src);
+
+    let mut errors = Vec::new();
+
+    let token_iter = Token::lexer(src).spanned().map(|(tok, span)| match tok {
+        Ok(tok) => (tok, span.into()),
+        Err(()) => (Token::Error, span.into()),
+    });
+
+    let token_stream = Stream::from_iter(token_iter).spanned((src.len()..src.len()).into());
+
+    let (parser_output, parser_errors) = parser().parse(token_stream).into_output_errors();
+
+    errors.extend(parser_errors.into_iter());
+
+    if let Some(token_tree) = parser_output {
+        println!("parsed program {:#?}", token_tree.0);
+
+        if errors.len() == 0 {
+            match TokenTree::eval(&token_tree) {
+                Ok(val) => println!("Return value: {}", val),
+                Err(e) => errors.push(Rich::custom(e.span, e.msg)),
+            }
+        }
+    }
+
+    for err in errors {
+        Report::build(ReportKind::Error, (), err.span().start)
+            .with_message(err.to_string())
+            .with_label(
+                Label::new(err.span().into_range())
+                    .with_message(err.reason().to_string())
+                    .with_color(Color::Red),
+            )
+            .with_labels(err.contexts().map(|(label, span)| {
+                Label::new(span.into_range())
+                    .with_message(format!("while parsing this {}", label))
+                    .with_color(Color::Yellow)
+            }))
+            .finish()
+            .eprint(Source::from(src))
+            .unwrap();
+    }
+}
+
+const SOURCES: [&str; 3] = [
+    r"
     (-
         (* (+ 4 7.3) 7)
         (/ 5 3)
     )
-";
+",
+    r"
+    (-
+        (* (+ 4 7.3) 7)
+        (/ 5 3
+    )
+",
+    r"
+    (-
+        (* (+ 4 7A3) 7)
+        (/ 5 3)
+    )
+",
+];
 
 fn main() {
-    println!("{}", SRC);
+    for (i, src) in SOURCES.iter().enumerate() {
+        run_program(src);
 
-    // Create a logos lexer over the source code
-    let token_iter = Token::lexer(SRC)
-        .spanned()
-        // Convert logos errors into tokens. We want parsing to be recoverable and not fail at the lexing stage, so
-        // we have a dedicated `Token::Error` variant that represents a token error that was previously encountered
-        .map(|(tok, span)| match tok {
-            // Turn the `Range<usize>` spans logos gives us into chumsky's `SimpleSpan` via `Into`, because it's easier
-            // to work with
-            Ok(tok) => (tok, span.into()),
-            Err(()) => (Token::Error, span.into()),
-        });
-
-    // Turn the token iterator into a stream that chumsky can use for things like backtracking
-    let token_stream = Stream::from_iter(token_iter)
-        // Tell chumsky to split the (Token, SimpleSpan) stream into its parts so that it can handle the spans for us
-        // This involves giving chumsky an 'end of input' span: we just use a zero-width span at the end of the string
-        .spanned((SRC.len()..SRC.len()).into());
-
-    // Parse the token stream with our chumsky parser
-    match parser().parse(token_stream).into_result() {
-        // If parsing was successful, attempt to evaluate the s-expression
-        Ok(sexpr) => match dbg!(sexpr).eval() {
-            Ok(out) => println!("Result = {}", out),
-            Err(err) => println!("Runtime error: {}", err),
-        },
-        // If parsing was unsuccessful, generate a nice user-friendly diagnostic with ariadne. You could also use
-        // codespan, or whatever other diagnostic library you care about. You could even just display-print the errors
-        // with Rust's built-in `Display` trait, but it's a little crude
-        Err(errs) => {
-            for err in errs {
-                Report::build(ReportKind::Error, (), err.span().start)
-                    .with_code(3)
-                    .with_message(err.to_string())
-                    .with_label(
-                        Label::new(err.span().into_range())
-                            .with_message(err.reason().to_string())
-                            .with_color(Color::Red),
-                    )
-                    .finish()
-                    .eprint(Source::from(SRC))
-                    .unwrap();
-            }
+        if i < SOURCES.len() - 1 {
+            println!("\n--\n");
         }
     }
 }
